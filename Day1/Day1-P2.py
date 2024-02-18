641b8493e1e8c35b815c09b04a6a45e89636251c
Sebastian R <sebreddy@gmail.com>
2024-02-18 16:44:16 +0000

    Completed Day 1 Part 2

---
+++ Day1/Day1-P2.py
@@ -0,0 +1,64 @@
+#Example 
+'''
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+
+29, 83, 13, 24, 42, 14, 76
+=>281
+'''
+
+keys = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+def example():
+    example_input = ["two1nine", "eightwothree", "abcone2threexyz", "xtwone3four", "4nineeightseven2", "zoneight234", "7pqrstsixteen"]
+
+    # Probably very inefficient, but don't know how to do it better
+    numbers = []
+
+    for ex in example_input:
+        out = []
+        for c, char in enumerate(ex):
+            for k, key in enumerate(keys):
+                l = len(key)
+
+                if ex[c: c+l] == key:
+                    out.append(values[k])
+        numbers.append(int(str(out[0]) + str(out[-1])))
+
+    print(numbers)
+    print(sum(numbers))
+
+
+def actual():
+    # Code to run on actual challenge
+
+    input_full = []
+    with open("input.txt", "r") as f:
+        input_full = f.readlines()
+
+    numbers = []
+
+    for ex in input_full:
+        out = []
+        for c, char in enumerate(ex):
+            for k, key in enumerate(keys):
+                l = len(key)
+
+                if ex[c: c+l] == key:
+                    out.append(values[k])
+        numbers.append(int(str(out[0]) + str(out[-1])))
+
+    print(numbers)
+    print(sum(numbers))
+
+
+
+#example()
+actual()    
